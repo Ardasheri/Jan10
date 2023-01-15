0e9521a1b04e051b965f2d0420596ec80a262517
Ardasheri <ardajon@gmail.com>
2023-01-14 23:36:43 -0500

    commit

---
+++ src/Homeworks/Jan14HomeWorkTask6.java
@@ -0,0 +1,28 @@
+package Homeworks;
+
+public class Jan14HomeWorkTask6 {
+    /*
+  6) Print the following pattern:
+*
+
+*
+
+*
+**
+*
+**
+*
+     */
+    public static void main(String[] args) {
+
+
+        System.out.println("*");
+        System.out.println();
+        System.out.println("*");
+        System.out.println();
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("*");
+    }}
