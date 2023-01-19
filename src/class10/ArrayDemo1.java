2b08199a9069a7534e29f1f9c2b11eed9773b60d
Ardasheri <ardajon@gmail.com>
2023-01-18 22:34:48 -0500

    commit

---
+++ src/class10/ArrayDemo1.java
@@ -0,0 +1,9 @@
+package class10;
+
+public class ArrayDemo1 {
+    public static void main(String[] args) {
+
+
+        System.out.println();
+    }
+}
--- src/replsPractice/repl35.java
+++ src/replsPractice/repl35.java
@@ -3,27 +3,16 @@ package replsPractice;
 import java.util.Scanner;
 
 public class repl35 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Do you need a loan? (True/False) ");
-        boolean needLoan = sc.nextBoolean();
 
-        if (needLoan) {
-            System.out.print("What is your credit score? ");
-            int creditScore = sc.nextInt();
-            String eligibility;
-            if (creditScore < 600) {
-                eligibility = "Not eligible";
-            } else if (creditScore <= 700) {
-                eligibility = "Maybe eligible";
-            } else if (creditScore <= 800) {
-                eligibility = "Eligible";
-            } else {
-                eligibility = "Definitely eligible";
-            }
-            System.out.println("Your loan eligibility is: " + eligibility);
-        } else {
-            System.out.println("Your loan eligibility is Unknown");
+    public static void main(String[] args) {
+        int m=2, total=0;
+
+        while(m<6) {
+
+            total=total+m;
+
+            m++;
+
         }
-    }
-}
+
+        System.out.print(total);}}
